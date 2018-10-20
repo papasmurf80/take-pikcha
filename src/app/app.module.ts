ac4241a28232ace809614de3eb5a7004cea3619c
joey atwell <joeyatwell@gmail.com>
2018-10-20 13:16:26 -0700

    created image provider, imported ionic storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { LoginRegisterPage } from '../pages/login-register/login-register';
+import { IonicStorageModule } from '@ionic/storage';
+
 
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ImageProvider } from '../providers/image/image';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -35,7 +39,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ImageProvider
   ]
 })
 export class AppModule {}
