51ea7cd0d36e543695e986bf3b0867d8bc123e6f
joey atwell <joeyatwell@gmail.com>
2018-10-22 21:40:45 -0700

    capture and display working, cleared storage on load 1x - function still exists in service for future use

--- src/providers/image/image.ts
+++ src/providers/image/image.ts
@@ -1,6 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
-import { IonicStorageModule } from '@ionic/storage';
+
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+class Photo {
+  data: any;
+}
 
 
 /*
@@ -11,9 +16,46 @@ import { IonicStorageModule } from '@ionic/storage';
 */
 @Injectable()
 export class ImageProvider {
+  public photos: Photo[] = [];
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ImageProvider Provider');
-  }
+  constructor(private camera: Camera, private storage: Storage) 
+    {console.log('Hello ImageProvider Provider');
+    }
 
-}
+    loadSaved() {
+      this.storage.get('photos').then((photos) => {
+        this.photos = photos || [];
+      });
+    }
+    cleanSlate(){
+      this.storage.clear().then( _ => {
+        console.log('all clear')
+      }
+      )
+    }
+  
+    public takePicture() {
+      console.log('wtf')
+      // Create options for the Camera Dialog
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+      };
+      
+      this.camera.getPicture(options).then((imageData) => {
+        //Add new photo to gallery
+        this.photos.unshift({
+          data: 'data:image/jpeg;base64,' + imageData 
+        });
+       
+       //View photos later
+       this.storage.set('photos', this.photos);
+      }, (err) => {
+        // Handle error
+        console.log('Camera issue:' + err)
+       });
+  
+    }
+  }    
